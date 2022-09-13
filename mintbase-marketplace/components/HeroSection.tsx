5daf6ec119d024eeb75e041d1a60d87ebc6dce83
luis pimenta <luis.pimenta.it@gmail.com>
2022-09-13 22:38:00 +0100

    init repo

---
+++ mintbase-marketplace/components/HeroSection.tsx
@@ -0,0 +1,15 @@
+import { MbText } from 'mintbase-ui';
+
+function HeroSection(): JSX.Element {
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center">
+      <div className="items-center justify-center">
+        <MbText className="text-3xl uppercase p-4 border-gray-100">
+          ECOTECH
+        </MbText>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
